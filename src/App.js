15a7fe0bbf2f45042cb3ae94834980ea68cae154
KR-2402 <karthika.jaya4@gmail.com>
2023-05-06 00:59:29 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import "./App.css"; 
 import Home from "./Components/Home";
-import Footer from "./Components/Footer";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import Footer from "./Components/Footer";
 import MovieRecc from "./Components/movierecc";
-import { BrowserRouter,Route, Routes} from "react-router-dom";
 
 function App() {
  
   return (
     <div className="App">
-      
-      {/* <Work />
-      <Testimonial />
-      <Contact /> */}
-      {/*Footer */}
-    
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Login/>}/>
-        <Route path="/Footer" element={<Footer />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/MovieRecc" element={<MovieRecc />} />
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/Home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/Signup" element={<Signup />} />
+              <Route path="/MovieRecc" element={<MovieRecc />} />
 
-      
-    </Routes>
-  </BrowserRouter>
-  </div>
+
+            </Routes>
+          </UserAuthContextProvider>
+     </div>
   );
 }
 
